28c4db4b206398ed68cf25110878bffc76b53639
singchia <singchia@163.com>
2026-01-09 21:02:52 +0800

    feat: remove old web

--- web/src/assets/icons/index.ts
+++
@@ -1,7 +0,0 @@
-const requireAll = (requireContext: any) => {
-  return requireContext.keys().map(requireContext);
-};
-
-const req = (require as any).context('@/assets/icons', true, /\.svg$/);
-
-requireAll(req);
